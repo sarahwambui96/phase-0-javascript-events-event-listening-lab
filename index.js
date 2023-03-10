c939b0ee97553d5a0dac507a38c40e754d627a06
sarahwambui96 <sarahwambui843@gmail.com>
2023-03-11 00:02:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+// binds an event listener in addingEventListener():
+const input = document.getElementById("button")
+
 function addingEventListener() {
+    alert("I was clicked");
+    input.addEventListener("click", addingEventListener)
 }
+//Binding a click event
